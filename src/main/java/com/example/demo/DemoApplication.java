24c9fe9be0907113dad4bc4b950ea89abd6d2e3f
heemabhatt <heema.bhatt@gmail.com>
2020-08-18 13:40:39 -0400

    update pom file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.controller"})
+@SpringBootApplication(scanBasePackages= {"com.example.controller"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }

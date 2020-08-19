51bc93efb4c852b773a984c5ec13465aac6bb853
heemabhatt <heema.bhatt@gmail.com>
2020-08-19 12:35:25 -0400

    added application.yml

--- src/main/java/com/example/controller/ApiController.java
+++ src/main/java/com/example/controller/ApiController.java
@@ -1,14 +1,20 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+	@Value("${myvariables.mykey}")
+	private String mykey;
+	
+	@Value("${myvariables.myenv}")
+	private String myenv;
 	@GetMapping("/")
 	public String Names()
 	{
-		return "Heema";
+		return "My Key:  " + mykey +  " My Env: " + myenv;
 	}
 }

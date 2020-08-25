b11f36a3ce6239aaa97b41f12cb5b05353e03972
Heema Bhatt <Heema.Bhatt@ey.com>
2020-08-25 14:58:32 -0400

    updated environment variable

--- src/main/java/com/example/controller/ApiController.java
+++ src/main/java/com/example/controller/ApiController.java
@@ -11,11 +11,13 @@ public class ApiController {
 	@Value("${spring.myenv}")
 	private String myenv;
 	
+	 @Value("${ENVIRONMENT_MESSAGE}")
+	 private String env_msg;
 	// run using this command : mvn clean install spring-boot:run -Dspring-boot.run.profiles=Local -DskipTests
 	
 	@GetMapping("/")
 	public String Names()
 	{
-		return " My Env: " + myenv   ;
+		return "***My Environment From Active Profile: " + myenv  + "***Environment Message From Config File: "+ env_msg + "***";
 	}
 }
